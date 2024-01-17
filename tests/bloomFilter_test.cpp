b355726560561b14e5f7717b37d01669d66e0b05
Eitan <eitanmaimoni1@gmail.com>
2024-01-17 13:23:55 +0200

    adding test and doing minor update to CMakeLists

--- tests/bloomFilter_test.cpp
+++ tests/bloomFilter_test.cpp
@@ -1,16 +1,64 @@
 #include <gtest/gtest.h>
 #include "../src/bloomFilter.cpp" // here we include the code to be tested
-//this test is to check if the bloom filter is working properly
 
-TEST(SumTest, TestAddUrl1) {
-    BloomFilter bloomFilter;
-    std::string url1 = "example.com";
-    bloomFilter.add_to_black_list(url1);
-    EXPECT_TRUE(bloomFilter.check_url(url1));
+//this test is to check if the bloom filter is working properly
+TEST(SanityCheck, ADD_URL) {
+    BloomFilter bloom_filter;
+    std::string url = "blacklist.com";
+    bloom_filter.addUrl(url);
+    EXPECT_TRUE(bloom_filter.check_url(url));
 }
 
-TEST(SumTest2, TestAddUrl2) {
-    BloomFilter bloomFilter;
-    std::string url2 = "notblacklisted.com";
-    EXPECT_FALSE(bloomFilter.check_url(url2));
+TEST(SanityCheck, NOT_BLACKLISTED_URL) {
+    BloomFilter bloom_filter;
+    std::string url = "notblacklisted.com";
+    EXPECT_FALSE(bloom_filter.check_url(url));
+}
+
+TEST(UNVALID_URL, ADD_BASIC_URL1) {
+    BloomFilter bloom_filter;  
+    std::string url = "imNotURL.com";
+    ASSERT_THROW(bloom_filter.add_to_black_list(url), std::invalid_argument);
+}
+
+TEST(UNVALID_URL, ADD_BASIC_URL2) {
+    BloomFilter bloom_filter;
+    std::string url = ".imNotURL.com";
+    ASSERT_THROW(bloom_filter.add_to_black_list(url), std::invalid_argument);
+}
+
+TEST(UNVALID_URL, ADD_BASIC_URL3) {
+    BloomFilter bloom_filter;
+    std::string url = "www..com";
+    ASSERT_THROW(bloom_filter.add_to_black_list(url), std::invalid_argument);
+}
+
+TEST(UNVALID_URL, ADD_BASIC_URL4) {
+    BloomFilter bloom_filter;
+    std::string url = "www.com";
+    ASSERT_THROW(bloom_filter.add_to_black_list(url), std::invalid_argument);
+}
+
+TEST(UNVALID_URL, ADD_BASIC_URL5) {
+    BloomFilter bloom_filter;
+    std::string url = "www.imNotURL.";
+    ASSERT_THROW(bloom_filter.add_to_black_list(url), std::invalid_argument);
+}
+
+TEST(UNVALID_URL, ADD_BASIC_URL6) {
+    BloomFilter bloom_filter;
+    std::string url = "www.imNotURL";
+    ASSERT_THROW(bloom_filter.add_to_black_list(url), std::invalid_argument);
+}
+
+TEST(UNVALID_URL, CHECK_BASIC_URL1) {
+    BloomFilter bloom_filter;
+    std::string url = "imNotURL.com";
+    ASSERT_THROW(bloom_filter.check_url(url), std::invalid_argument);
+}
+
+TEST(UNVALID_URL, DOUBLE_CHECK_BASIC_URL1) {
+    BloomFilter bloom_filter;
+    std::string url = "www.imNotURL";
+    ASSERT_THROW(bloom_filter.double_check_url(url), std::invalid_argument);
 }
