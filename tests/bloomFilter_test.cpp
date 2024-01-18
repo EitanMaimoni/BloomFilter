a53e725ecaaa1f6a6ea876d719cbe9a6d42ce77c
Ori Levy <orile14@gmail.com~>
2024-01-18 15:08:17 +0200

    new tests

--- tests/bloomFilter_test.cpp
+++ tests/bloomFilter_test.cpp
@@ -6,11 +6,25 @@
 TEST(SanityCheck, ADD_URL) {
     BloomFilter bloom_filter(100, 1);
     std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
     bloom_filter.add_url_to_bloomFilter(url);
     EXPECT_TRUE(bloom_filter.is_on_bit_array(url));
+    EXPECT_FALSE(bloom_filter.is_on_black_list(url1));
 }
 TEST(SanityCheck, ADD_URL1) {
     BloomFilter bloom_filter(100, 1);
+    std::string url = "www.blacklist.com";
+    bloom_filter.add_url_to_bloomFilter(url);
+    EXPECT_TRUE(bloom_filter.is_on_black_list(url));
+}
+TEST(SanityCheck, ADD_URL12) {
+    BloomFilter bloom_filter(100, 1,2);
+    std::string url = "www.blacklist.com";
+    bloom_filter.add_url_to_bloomFilter(url);
+    EXPECT_TRUE(bloom_filter.is_on_bit_array(url));
+}
+TEST(SanityCheck, ADD_URL122) {
+    BloomFilter bloom_filter(100, 1,2);
     std::string url = "www.blacklist.com";
     bloom_filter.add_url_to_bloomFilter(url);
     EXPECT_TRUE(bloom_filter.is_on_black_list(url));
