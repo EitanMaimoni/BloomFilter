b0c932d2d732bb0c71f1ec6406284bba92dbeaaa
Ori Levy <orile14@gmail.com~>
2024-01-18 15:21:03 +0200

    adding tests

--- tests/bloomFilter_test.cpp
+++ tests/bloomFilter_test.cpp
@@ -1,31 +1,69 @@
 #include <gtest/gtest.h>
 #include "../src/bloomFilter.h" // here we include the code to be tested
 #include "../src/hFunc.h" // here we include the code to be tested
-
+int length=64;
 //checking if we can add a url to the black list
 TEST(SanityCheck, ADD_URL) {
-    BloomFilter bloom_filter(100, 1);
+    BloomFilter bloom_filter(length, 1);
     std::string url = "www.blacklist.com";
     std::string url1 = "www.blacklist1.com";
     bloom_filter.add_url_to_bloomFilter(url);
     EXPECT_TRUE(bloom_filter.is_on_bit_array(url));
-    EXPECT_FALSE(bloom_filter.is_on_black_list(url1));
+    EXPECT_FALSE(bloom_filter.is_on_bit_array(url1));
 }
 TEST(SanityCheck, ADD_URL1) {
-    BloomFilter bloom_filter(100, 1);
+    BloomFilter bloom_filter(length, 1);
     std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
     bloom_filter.add_url_to_bloomFilter(url);
     EXPECT_TRUE(bloom_filter.is_on_black_list(url));
+    EXPECT_FALSE(bloom_filter.is_on_black_list(url1));
 }
-TEST(SanityCheck, ADD_URL12) {
-    BloomFilter bloom_filter(100, 1,2);
+TEST(SanityCheck, ADD_URL2) {
+    BloomFilter bloom_filter(length, 2);
     std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
     bloom_filter.add_url_to_bloomFilter(url);
     EXPECT_TRUE(bloom_filter.is_on_bit_array(url));
+    EXPECT_FALSE(bloom_filter.is_on_bit_array(url1));
 }
-TEST(SanityCheck, ADD_URL122) {
-    BloomFilter bloom_filter(100, 1,2);
+TEST(SanityCheck, ADD_URL3) {
+    BloomFilter bloom_filter(length, 2);
     std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
     bloom_filter.add_url_to_bloomFilter(url);
     EXPECT_TRUE(bloom_filter.is_on_black_list(url));
+    EXPECT_FALSE(bloom_filter.is_on_black_list(url1));
+}
+TEST(SanityCheck, ADD_URL4) {
+    BloomFilter bloom_filter(length, 1,2);
+    std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
+    bloom_filter.add_url_to_bloomFilter(url);
+    EXPECT_TRUE(bloom_filter.is_on_bit_array(url));
+    EXPECT_FALSE(bloom_filter.is_on_bit_array(url1));
+}
+TEST(SanityCheck, ADD_URL5) {
+    BloomFilter bloom_filter(length, 1,2);
+    std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
+    bloom_filter.add_url_to_bloomFilter(url);
+    EXPECT_TRUE(bloom_filter.is_on_black_list(url));
+    EXPECT_FALSE(bloom_filter.is_on_black_list(url1));
+}
+TEST(SanityCheck, ADD_URL6) {
+    BloomFilter bloom_filter(length, 2,1);
+    std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
+    bloom_filter.add_url_to_bloomFilter(url);
+    EXPECT_TRUE(bloom_filter.is_on_bit_array(url));
+    EXPECT_FALSE(bloom_filter.is_on_bit_array(url1));
+}
+TEST(SanityCheck, ADD_URL7) {
+    BloomFilter bloom_filter(length, 2,1);
+    std::string url = "www.blacklist.com";
+    std::string url1 = "www.blacklist1.com";
+    bloom_filter.add_url_to_bloomFilter(url);
+    EXPECT_TRUE(bloom_filter.is_on_black_list(url));
+    EXPECT_FALSE(bloom_filter.is_on_black_list(url1));
 }
