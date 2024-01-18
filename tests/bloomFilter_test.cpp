ee9ed1b6d27905468df2514a077c7f06f950e919
Ori Levy <orile14@gmail.com~>
2024-01-18 14:57:11 +0200

    updating tests and file linking

--- tests/bloomFilter_test.cpp
+++ tests/bloomFilter_test.cpp
@@ -1,73 +1,17 @@
 #include <gtest/gtest.h>
-#include "../src/bloomFilter.cpp" // here we include the code to be tested
+#include "../src/bloomFilter.h" // here we include the code to be tested
+#include "../src/hFunc.h" // here we include the code to be tested
 
 //checking if we can add a url to the black list
 TEST(SanityCheck, ADD_URL) {
     BloomFilter bloom_filter(100, 1);
     std::string url = "www.blacklist.com";
     bloom_filter.add_url_to_bloomFilter(url);
-    EXPECT_TRUE(bloom_filter.check_url(url));
+    EXPECT_TRUE(bloom_filter.is_on_bit_array(url));
 }
-
-//checking if unblacklisted url is not blacklisted
-TEST(SanityCheck, NOT_BLACKLISTED_URL) {
-    BloomFilter bloom_filter(10,1);
-    std::string url = "www.notblacklisted.com";
-    EXPECT_FALSE(bloom_filter.check_url(url));
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, ADD_BASIC_URL1) {
-    BloomFilter bloom_filter(5,1,2);  
-    std::string url = "imNotURL.com";
-    ASSERT_THROW(bloom_filter.add_url_to_bloomFilter(url), std::invalid_argument);
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, ADD_BASIC_URL2) {
-    BloomFilter bloom_filter(10,1,1);
-    std::string url = ".imNotURL.com";
-    ASSERT_THROW(bloom_filter.add_url_to_bloomFilter(url), std::invalid_argument);
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, ADD_BASIC_URL3) {
-    BloomFilter bloom_filter(10,1,1);
-    std::string url = "www..com";
-    ASSERT_THROW(bloom_filter.add_url_to_bloomFilter(url), std::invalid_argument);
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, ADD_BASIC_URL4) {
-    BloomFilter bloom_filter(10,1,1);
-    std::string url = "www.com";
-    ASSERT_THROW(bloom_filter.add_url_to_bloomFilter(url), std::invalid_argument);
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, ADD_BASIC_URL5) {
-    BloomFilter bloom_filter(5,2);
-    std::string url = "www.imNotURL.";
-    ASSERT_THROW(bloom_filter.add_url_to_bloomFilter(url), std::invalid_argument);
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, ADD_BASIC_URL6) {
-    BloomFilter bloom_filter(10,1,1);
-    std::string url = "www.imNotURL";
-    ASSERT_THROW(bloom_filter.add_url_to_bloomFilter(url), std::invalid_argument);
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, CHECK_BASIC_URL1) {
-    BloomFilter bloom_filter(10,1,1);
-    std::string url = "imNotURL.com";
-    ASSERT_THROW(bloom_filter.check_url(url), std::invalid_argument);
-}
-
-//checking if we cant add unvalid url to the black list
-TEST(UNVALID_URL, DOUBLE_CHECK_BASIC_URL1) {
-    BloomFilter bloom_filter(8,2,2);
-    std::string url = "www.imNotURL";
-    ASSERT_THROW(bloom_filter.check_url(url), std::invalid_argument);
+TEST(SanityCheck, ADD_URL1) {
+    BloomFilter bloom_filter(100, 1);
+    std::string url = "www.blacklist.com";
+    bloom_filter.add_url_to_bloomFilter(url);
+    EXPECT_TRUE(bloom_filter.is_on_black_list(url));
 }
--- tests/main_test.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-#include "../src/main.cpp"
-
-TEST(sanity,test1){
-    
-}
