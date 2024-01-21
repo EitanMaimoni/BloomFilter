26b53af44357903576ef7642b09dbcd889c708b3
Eitan <eitanmaimoni1@gmail.com>
2024-01-21 18:13:28 +0200

    passing test

--- src/oneHashBloomFilterValidator.cpp
+++ src/oneHashBloomFilterValidator.cpp
@@ -6,22 +6,22 @@
 // Default constructor
 oneHashBloomFilterValidator::oneHashBloomFilterValidator() {
     
-};
+}
 
 std::vector<int> oneHashBloomFilterValidator::validationCheck(std::string& line) {
-    std::vector<int> result(3);
+    std::vector<int> result(2);
+    std::vector<int> unValid(0);
 
     std::istringstream iss(line);
         
-    if (iss >> result[0] >> result[1] >> result[2]) {
-        if ((result[0] <= 0) || (result[1] != 1 && result[1] != 2) || (result[2] != 1 && result[2] != 2 && result[2] != result[1])) {
-            // X is not > 0 or Y is not 1 or 2 or Z ins not 1, 2, or equal to Y
-            return std::vector<int>();
+    if (iss >> result[0] >> result[1]) {
+        if ((result[0] <= 0) || (result[1] != 1 && result[1] != 2)) {
+            // X is not > 0 or Y is not 1 or 2
+            return unValid;
         }
 
         // If we get here, we successfully read X, Y, and Z
         return result;
     }
-
-    return result;
+    return unValid;
 };
--- tests/twoHashBloomFilterValidator_test.cpp
+++ tests/twoHashBloomFilterValidator_test.cpp
@@ -1,66 +1,68 @@
 #include <gtest/gtest.h>
-#include "../src/oneHashBloomFilterValidator.h"
+#include "../src/bloomFilterValidator.h"
+#include "../src/twoHashBloomFilterValidator.h"
 #include <vector>
 
-TEST(SanityCheck, VALID_INPUT) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT1) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "8 1 2";
     std::vector<int> expected = {8, 1, 2};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT1) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT2) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "64 1 2";
     std::vector<int> expected = {64, 1, 2};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT2) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT3) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "x y z";
     std::vector<int> expected = {};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT3) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT4) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "8 x 1";
     std::vector<int> expected = {};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT4) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT5) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "z";
     std::vector<int> expected = {};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT5) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT6) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "64";
     std::vector<int> expected = {};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT7) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT7) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "x 1 2";
     std::vector<int> expected = {};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT8) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT8) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "8 1 2 x c v v";
     std::vector<int> expected = {8, 1, 2};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
 
-TEST(SanityCheck, VALID_INPUT11) {
-    validInitInput check = validInitInput();
+TEST(SanityCheckTwoHash, VALID_INPUT9) {
+    twoHashBloomFilterValidator check = twoHashBloomFilterValidator();
     std::string url = "64 1 1 1 1 1 1 2";
     std::vector<int> expected = {64, 1, 2};
     EXPECT_EQ(check.validationCheck(url), expected);
 }
+
